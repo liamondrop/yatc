41a6788c679122c353cea6832ee4a4465d6870cc
Liam Bowers <liamondrop@gmail.com>
2015-11-08 18:01:28 -0500

    getting user lookup functional

--- application/static/javascripts/src/views/user-lookup-view.js
+++ application/static/javascripts/src/views/user-lookup-view.js
@@ -1,16 +1,34 @@
-import {bindAll} from 'underscore';
+import {bindAll, isEmpty} from 'underscore';
 import {View} from 'backbone';
 
 const UserLookupView = View.extend({
-  initialize() {
+  initialize(options) {
     bindAll(this, '_onFormSubmit');
+    this.options = options;
+    this.cacheElements();
+    this.setupEventListeners();
+  },
+
+  cacheElements() {
     this.$form = this.$('form');
+    this.$input = this.$('[name="screen-name"]');
+  },
+
+  setupEventListeners() {
     this.$form.on('submit', this._onFormSubmit);
   },
 
   _onFormSubmit(e) {
     e.preventDefault();
-    console.log('submit')
+    const {statusCollection} = this.options;
+    const screenName = this.$input.val();
+
+    try {
+      statusCollection.setProp('screenName', screenName);
+      statusCollection.fetch();
+    } catch (ex) {
+      console.log(ex)
+    }
   }
 });
 
