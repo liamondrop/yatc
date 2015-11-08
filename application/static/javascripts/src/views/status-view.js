2b03f0709705dbcc18349468f536fdf7db818a6a
Liam Bowers <liamondrop@gmail.com>
2015-11-08 18:01:50 -0500

    basic status view

--- application/static/javascripts/src/views/status-view.js
+++ application/static/javascripts/src/views/status-view.js
@@ -1,7 +1,16 @@
+import _ from 'underscore';
 import {View} from 'backbone';
+import templateStr from '../templates/panel-body.html';
 
 const StatusView = View.extend({
-  initialize() {}
+  className: 'panel panel-default',
+
+  template: _.template(templateStr),
+
+  render() {
+    this.$el.html(this.template({text: 'SOME TEXT'}));
+    return this;
+  }
 });
 
 export default StatusView;
