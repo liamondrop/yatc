eb4da8bfe148f195ab5ac5265d9e371d5df3d470
Liam <liamondrop@gmail.com>
2015-11-10 02:16:30 -0500

    returning some helpful date time props from the server

--- application/views.py
+++ application/views.py
@@ -1,14 +1,12 @@
-import time
-import datetime
+from datetime import datetime
 from application import app, client
 from flask import render_template, jsonify
 from werkzeug.exceptions import HTTPException
 
+DT_FORMAT = '%a %b %d %H:%M:%S +0000 %Y'
 
 @app.route('/')
 def index():
-    """dump the data into the template on initial pageload
-        for fast rendering"""
     return render_template('partials/_root.html')
 
 
@@ -18,9 +16,11 @@ def user_timeline(screen_name):
     try:
         response = client.api.statuses.user_timeline.get(
             screen_name=screen_name, count=10)
-        return jsonify(statuses=normalize_statuses(response.data))
+        now = datetime.utcnow()
+        return jsonify(retrieved_at=now.strftime(DT_FORMAT),
+            statuses=normalize_statuses(response.data))
     except Exception as ex:
-        # format exceptions as json for easy digestibility
+        # format exceptions as json response for easy digestibility
         response = jsonify(message=str(ex))
         response.status_code = (ex.code
                                 if isinstance(ex, HTTPException)
@@ -36,10 +36,10 @@ def test():
 
 def normalize_statuses(statuses=[]):
     """normalize the status response for easy Backbone consumption"""
-    get_time_delta(statuses[0])
     return [{'id': status.id,
              'name': status.user.name,
              'created_at': status.created_at,
+             'created_at_nice': get_time_delta(status.created_at),
              'retweet_count': status.retweet_count,
              'screen_name': status.user.screen_name,
              'profile_image_url': status.user.profile_image_url,
@@ -47,6 +47,19 @@ def normalize_statuses(statuses=[]):
 
 
 def get_time_delta(date_str):
-    print "########################\n"
-    print time.strptime(date_str)
-    print "\n########################"
+    curr = datetime.utcnow()
+    prev = datetime.strptime(date_str, DT_FORMAT)
+    delta = curr - prev
+
+    if delta.days > 364:
+        return prev.strftime('%b %e, %Y')
+    if delta.days > 2:
+        return prev.strftime('%b %e')
+    if delta.days > 0:
+        return '{}d'.format(delta.days)
+    if delta.seconds / 3600 > 1:
+        return '{}h'.format(delta.seconds / 3600)
+    if delta.seconds / 60 > 1:
+        return '{}m'.format(delta.seconds / 60)
+    return '{}s'.format(delta.seconds)
+
