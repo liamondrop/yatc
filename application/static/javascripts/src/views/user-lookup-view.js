608394874eedf500fdbe3f210fe2075eb0177ae5
Liam <liamondrop@gmail.com>
2015-11-10 04:27:52 -0500

    adding collection timer event handling

--- application/static/javascripts/src/views/user-lookup-view.js
+++ application/static/javascripts/src/views/user-lookup-view.js
@@ -12,12 +12,18 @@ const UserLookupView = View.extend({
     this.$form = this.$('form');
     this.$input = this.$('input[name="screen-name"]');
     this.$button = this.$('button[type="submit"]');
+    this.$lastUpdate = this.$('#last-update');
+    this.$interval = this.$('#last-update > .interval');
+    this.$event = this.$('#last-update > .event');
   },
 
   setupEventListeners(collection) {
     this.$form.on('submit', this._onFormSubmit);
     this.listenTo(collection, 'request', this._onCollectionRequest);
     this.listenTo(collection, 'sync error', this._onCollectionComplete);
+    this.listenTo(collection, 'timer:start', this._onTimerStart);
+    this.listenTo(collection, 'timer:increment', this._onTimerIncrement);
+    this.listenTo(collection, 'timer:end', this._onTimerEnd);
   },
 
   teardownEventListeners() {
@@ -40,13 +46,26 @@ const UserLookupView = View.extend({
     this.$el.addClass('working');
     this.$input.prop('disabled', true);
     this.$button.prop('disabled', true);
+    this.$lastUpdate.addClass('in');
+    this.$event.text('Fetching').addClass('in');
   },
 
   _onCollectionComplete() {
     this.$el.removeClass('working');
     this.$input.prop('disabled', false);
     this.$button.prop('disabled', false);
-  }
+    this.$event.text('Done');
+  },
+
+  _onTimerIncrement(delta) {
+    const timeAgo = `${Math.round(delta / 1000)}s ago`;
+    this.$event.text('Last updated');
+    this.$interval.text(timeAgo).addClass('in');
+  },
+
+  _onTimerEnd(...args) {
+    this.$interval.removeClass('in');
+  },
 });
 
 export default UserLookupView;
